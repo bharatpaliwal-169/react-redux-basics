66b9405fab63533eb4ca3af2d7fe1bf1552f49a0
Bharat Paliwal <18uec169@lnmiit.ac.in>
2021-05-07 14:11:34 +0530

    Learning: action,reducer,store

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Hello From Index!!");
+// console.log("Hello From Index!!");
+
+//action 
+const BUY_CAKE = 'BUY_CAkE';
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info : 'First redux action'
+  }
+}
+
+//reducer
+
+// (prevState,action) => newState
+const initialState = {
+  numOfCakes: 10
+}
+
+const reducer = (state = initialState , action) =>{
+  switch(action.type){
+    case BUY_CAKE: return {
+      ...state,
+      numOfCakes: state.numOfCakes - 1
+    }
+    default : return state
+  }
+}
